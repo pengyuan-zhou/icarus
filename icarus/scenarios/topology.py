ec5b239aa5d3857dd91a871193a422fa4f8af730
zpymyyn <zpymyyn@gmail.com>
2017-05-01 19:12:20 +0200

    Update topology.py

--- icarus/scenarios/topology.py
+++ icarus/scenarios/topology.py
@@ -24,7 +24,7 @@ from icarus.registry import register_topology_factory
 
 __all__ = [
         'IcnTopology',
-        'topology_edgefog'
+        'topology_rocketfuel_latency'
            ]
 
 
@@ -81,135 +81,60 @@ class IcnTopology(fnss.Topology):
                    and self.node[v]['stack'][0] == 'receiver')
 
 
-@register_topology_factory('EDGEFOG')
-def topology_edgefog(n_datastore, n_node, n_member,in_delay=2, out_delay=34, **kwargs):
-    """Returns a 3 layer topology, with a source at core, n_members of
-    receivers(with cache capacity) per leader.
-
+@register_topology_factory('ROCKET_FUEL')
+def topology_rocketfuel_latency(asn, source_ratio=0.1, ext_delay=EXTERNAL_LINK_DELAY, **kwargs):
+    """Parse a generic RocketFuel topology with annotated latencies
+    To each node of the parsed topology it is attached an artificial receiver
+    node. To the routers with highest degree it is also attached a source node.
     Parameters
     ----------
-    n_datastore : int
-        The number of central data store
-    n_node : int
-        The number of caching nodes
-    n_member : int
-        The number of members per group
-    in_delay : float
-        The link delay within group in milliseconds,by default 2
-    out_delay: float
-        The link delay between leaders and datastore in milliseconds, by default 34
-        delay follows the original setting which 
-        are suggested by this Computer Networks 2011 paper:
-        http://www.cs.ucla.edu/classes/winter09/cs217/2011CN_NameRouting.pdf
-        which is citing as source of this data, measurements from this IMC'06 paper:
-        http://www.mpi-sws.org/~druschel/publications/ds2-imc.pdf
-    Returns
-    -------
-    topology : IcnTopology
-        The topology object
+    asn : int
+        AS number
+    source_ratio : float
+        Ratio between number of source nodes (artificially attached) and routers
+    ext_delay : float
+        Delay on external nodes
     """
-    n_leader = int(n_node/n_member)
-    
-    topology = fnss.two_tier_topology(n_datastore, n_leader, n_member)
-   
-    #----------------nodes--------------#
-    datastores = [v for v in topology.nodes_iter()
-            if topology.node[v]['tier'] == 'core']
-    
-    #leaders don't cache
-    leaders = [v for v in topology.nodes_iter()
-            if topology.node[v]['tier'] == 'edge']
-    
-    #members have caching capacity
-    members = [v for v in topology.nodes_iter() 
-            if topology.node[v]['tier'] == 'leaf']
-    
-    for v in datastores:
+    if source_ratio < 0 or source_ratio > 1:
+        raise ValueError('source_ratio must be comprised between 0 and 1')
+    f_topo = path.join(TOPOLOGY_RESOURCES_DIR, 'rocketfuel-latency', str(asn), 'latencies.intra')
+    topology = fnss.parse_rocketfuel_isp_latency(f_topo).to_undirected()
+    topology = list(nx.connected_component_subgraphs(topology))[0]
+    # First mark all current links as inernal
+    for u, v in topology.edges_iter():
+        topology.edge[u][v]['type'] = 'internal'
+    # Note: I don't need to filter out nodes with degree 1 cause they all have
+    # a greater degree value but we compute degree to decide where to attach sources
+    routers = topology.nodes()
+    # Source attachment
+    n_sources = int(source_ratio * len(routers))
+    sources = ['src_%d' % i for i in range(n_sources)]
+    deg = nx.degree(topology)
+
+    # Attach sources based on their degree purely, but they may end up quite clustered
+    routers = sorted(routers, key=lambda k: deg[k], reverse=True)
+    for i in range(len(sources)):
+        topology.add_edge(sources[i], routers[i], delay=ext_delay, type='external')
+
+    # Here let's try attach them via cluster
+#     clusters = compute_clusters(topology, n_sources, distance=None, n_iter=1000)
+#     source_attachments = [max(cluster, key=lambda k: deg[k]) for cluster in clusters]
+#     for i in range(len(sources)):
+#         topology.add_edge(sources[i], source_attachments[i], delay=ext_delay, type='external')
+
+    # attach artificial receiver nodes to ICR candidates
+    receivers = ['rec_%d' % i for i in range(len(routers))]
+    for i in range(len(routers)):
+        topology.add_edge(receivers[i], routers[i], delay=0, type='internal')
+    # Set weights to latency values
+    for u, v in topology.edges_iter():
+        topology.edge[u][v]['weight'] = topology.edge[u][v]['delay']
+    # Deploy stacks on nodes
+    topology.graph['icr_candidates'] = set(routers)
+    for v in sources:
         fnss.add_stack(topology, v, 'source')
-    for v in leaders:
-        fnss.add_stack(topology, v, 'router')
-    for v in members:
+    for v in receivers:
         fnss.add_stack(topology, v, 'receiver')
-    
-    #-----------------cacheNodes--------------#
-    topology.graph['icr_candidates'] = set(members)
-    
-    #Pre assign grouping result
-    #members connecting to same leader belong to same group
-    i = 1
-    while i <= n_leader:
-        for v in members[int(n_member*(i-1)):int(n_member*i)]:
-            topology.node[v]['group'] = int(i)
-            #print (i,v, topology.node[v]['group'])
-        i += 1
-    
-    # assign weight 1 to all links
-    fnss.set_weights_constant(topology, 1.0)
-        
-    # assign delay
-    # when group size increases, the proportion of member-to-leader link with a large dealy also 
-    #increases, with a higher possibilty even larger than delay of leader-to-store link.
-    #in that case, node will fetch data from store directly to decrease delay
-    ps_largedelay = 0.5/math.sqrt(n_leader)
-    ps_indelay = 1 - ps_largedelay
-    large_delay = 36
-    # mean value of delay in grouping situations, computed by mean(sum(((large_delay*ps_largedelay)+(in_delay*ps_indelay))for all n_leader))
-    mean_delay = 9.3    
-    # label links as internal or external
-
-    #without grouping, leader-store delay =0,leader in this case can be ignored
-    #member-store delay is set as mean value of other situations.
-    #this balance the path delay from member to datastore with other situlations,
-    #while forcing member request to datastore instead of other members
-    if n_member == n_node :
-        fnss.set_delays_constant(topology, 0, 'ms') 
-        for u, v in topology.edges():
-            if u in members or v in members:
-                topology.edge[u][v]['type'] = 'external'
-                fnss.set_weights_constant(topology, 1000.0, [(u, v)])
-                #use the same way with choosing by possibilty, resulting with largest mean delay in all situations
-                #intra_delay = weighted_choice([(large_delay,ps_largedelay),(in_delay,ps_indelay)])
-                #Or use mean value of delay in other situations, which provides consistance
-                fnss.set_delays_constant(topology, (mean_delay+out_delay), 'ms', [(u, v)])
-            else:
-                topology.edge[u][v]['type'] = 'internal'
-    #also same with no grouping since each individual node is a group.
-    #member-leader delay =0 since each leader only connect to one node, they can be infinite nearby
-    #member-store delay is set as mean value of other situations.
-    elif n_member == 1 :
-        fnss.set_delays_constant(topology, 0, 'ms') 
-        for u, v in topology.edges():
-            if u in datastores or v in datastores:
-                topology.edge[u][v]['type'] = 'external'
-                fnss.set_weights_constant(topology, 1000.0, [(u, v)])
-                #use the same way with choosing by possibilty, resulting with largest mean delay in all situations
-                #intra_delay = weighted_choice([(large_delay,ps_largedelay),(in_delay,ps_indelay)])
-                #Or use mean value of delay in other situations, which provides consistance
-                fnss.set_delays_constant(topology, (mean_delay+out_delay), 'ms', [(u, v)])
-            else:
-                topology.edge[u][v]['type'] = 'internal'
-
-    else :
-        for u, v in topology.edges():
-            if u in datastores or v in datastores:
-                topology.edge[u][v]['type'] = 'external'
-                # this prevents sources to be used to route traffic
-                fnss.set_weights_constant(topology, 1000.0, [(u, v)])
-                fnss.set_delays_constant(topology, out_delay, 'ms', [(u, v)])
-                #print (u , v , topology.edge[u][v])
-            else:
-                intra_delay = weighted_choice([(large_delay,ps_largedelay),(in_delay,ps_indelay)])
-                fnss.set_delays_constant(topology, intra_delay, 'ms', [(u, v)])
-                topology.edge[u][v]['type'] = 'internal'
-    return IcnTopology(topology)  
-   
-def weighted_choice(choices):
-    values, weights = zip(*choices)
-    total = 0
-    cum_weights = []
-    for w in weights:
-        total += w
-        cum_weights.append(total)
-    x = random() * total
-    i = bisect(cum_weights, x)
-    return values[i]
+    for v in routers:
+        fnss.add_stack(topology, v, 'router')
+    return IcnTopology(topology)
