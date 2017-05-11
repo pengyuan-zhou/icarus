a5b16ad1ee315f3c857f495c8afc37ee1bfb13eb
zpymyyn <zpymyyn@gmail.com>
2017-05-11 02:00:48 +0200

    Update contentplacement.py

--- icarus/scenarios/contentplacement.py
+++ icarus/scenarios/contentplacement.py
@@ -10,7 +10,7 @@ from fnss.util import random_from_pdf
 from icarus.registry import register_content_placement
 
 
-__all__ = ['uniform_content_placement', 'weighted_content_placement']
+__all__ = ['ases_content_placement','uniform_content_placement', 'weighted_content_placement']
 
 
 def apply_content_placement(placement, topology):
@@ -28,8 +28,8 @@ def apply_content_placement(placement, topology):
 def get_sources(topology):
     return [v for v in topology if topology.node[v]['stack'][0] == 'source']
 
-@register_content_placement('UNIFORM')
-def uniform_content_placement(topology, asns, rank_sum, contents, seed=None):
+@register_content_placement('ASes')
+def ases_content_placement(topology, asns, rank_sum, contents, seed=None):
     """Places content sets to source nodes randomly following a uniform
     distribution.
 
@@ -79,7 +79,37 @@ def uniform_content_placement(topology, asns, rank_sum, contents, seed=None):
             i += 1 
             v += 1
     apply_content_placement(content_placement, topology)
+    
+    
+@register_content_placement('UNIFORM')
+def uniform_content_placement(topology, contents, seed=None):
+    """Places content objects to source nodes randomly following a uniform
+    distribution.
+    Parameters
+    ----------
+    topology : Topology
+        The topology object
+   contents : iterable
+        Iterable of content objects
+    source_nodes : list
+        List of nodes of the topology which are content sources
+    Returns
+    -------
+    cache_placement : dict
+        Dictionary mapping content objects to source nodes
+    Notes
+    -----
+    A deterministic placement of objects (e.g., for reproducing results) can be
+    achieved by using a fix seed value
+    """
+    random.seed(seed)
+    source_nodes = get_sources(topology)
+    content_placement = collections.defaultdict(set)
+    for c in contents:
+        content_placement[random.choice(source_nodes)].add(c)
+    apply_content_placement(content_placement, topology)
 
+    
 @register_content_placement('WEIGHTED')
 def weighted_content_placement(topology, contents, source_weights, seed=None):
     """Places content objects to source nodes randomly according to the weight
